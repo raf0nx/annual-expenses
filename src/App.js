6c258dc275098045d064a03c8168cb607e9ec9ba
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-12-23 02:30:15 +0100

    style: Reorganize file structure and change "normal" functions to arrow funcs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
 	const expenses = [
 		{
 			id: "e1",
@@ -31,7 +31,7 @@ function App() {
 	return (
 		<div>
 			<h2>Let's get started!</h2>
-			<Expenses items={expenses}/>
+			<Expenses items={expenses} />
 		</div>
 	);
 }
