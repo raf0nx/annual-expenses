7ea47f3771ef209f985ffdbc1218b8dece20be56
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-12-27 16:32:08 +0100

    feat: Rendering expense form conditionally

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm";
+import Card from "../UI/Card";
 import "./NewExpense.css";
 
 const NewExpense = props => {
+	const [isEditing, setIsEditing] = useState(false);
+
 	const saveExpenseDataHandler = enteredExpenseData => {
 		const expenseData = {
 			...enteredExpenseData,
@@ -9,12 +14,25 @@ const NewExpense = props => {
 		};
 
 		props.onAddExpense(expenseData);
+		setIsEditing(false);
 	};
 
+	const startEditingHandler = () => setIsEditing(true);
+
+	const stopEditingHandler = () => setIsEditing(false);
+
 	return (
-		<div className="new-expense">
-			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-		</div>
+		<Card className="new-expense">
+			{!isEditing && (
+				<button onClick={startEditingHandler}>Add New Expense</button>
+			)}
+			{isEditing && (
+				<ExpenseForm
+					onSaveExpenseData={saveExpenseDataHandler}
+					onCancel={stopEditingHandler}
+				/>
+			)}
+		</Card>
 	);
 };
 
