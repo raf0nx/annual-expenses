75bf3c28eb606b46171c62a117d58b552847edc4
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-12-27 16:03:05 +0100

    feat: ExpensesList component and showing expenses content conditionally

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpenseFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
@@ -21,14 +21,7 @@ const Expenses = props => {
 					pickedYear={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{filteredExpenses.map(expense => (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				))}
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
